557db47d88dae6eaf106adb832ee4755aa03a0b4
HPCarey <hannahpcarey@gmail.com>
2022-06-24 08:54:11 +0000

    fix: js comma error line 4

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Below are questions, options and answers for quiz
+const questions = [
+    {
+      question: "Meaning: Learn a new skill through practice rather than study."
+      options: ["read up on", "fill in", "pick up", "take up"],
+      correctAnswer: "pick up"
+    },
+    {
+      question: "Meaning: Revise/ study a concept again to clarify the meaning",
+      options: ["look up", "fill in", "read up on", "go over"],
+      correctAnswer: "go over"
+    },
+    {
+        question: "Meaning: Use an information database or learning resource to verify something or learn something specific",
+        options: ["read up on", "look up", "pick up", "take up"],
+        correctAnswer: "look up"
+      },
+      {
+        question: "Meaning: Start learning a new skill or hobby",
+        options: ["pick up", "read up on", "take up", "go over"],
+        correctAnswer: "take up"
+      },
+      {
+        question: "Meaning: Expand your knowledge of a subject through study",
+        options: ["read up on", "look up", "take up", "go over"],
+        correctAnswer: "read up on"
+      },
+      {
+        question: "Meaning: Complete information that is missing from a form or test",
+        options: ["look up", "fill in", "read up on", "take up"],
+        correctAnswer: "fill in"
+      },
+   ]
+   
