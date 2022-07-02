b097bb0dc1f42da5cf75debfe1bfe20e07a9d98f
HPCarey <hannahpcarey@gmail.com>
2022-07-02 12:12:41 +0000

    feat: add variables to shuffle and display questions and add to run quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // define variables to select elements
 const startButton = document.getElementById("start-quiz-button");
 const nextButton = document.getElementById("next-button");
-const question = document.getElementById("question");
+const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const startPage = document.getElementById("start-page");
 const quizContainer = document.getElementById("quiz-container");
 const welcomeHeading = document.getElementById("welcome-heading");
+
+let shuffledQuestions
+let currentQuestionIndex
 
 // event listeners
 startButton.addEventListener('click', runQuiz);
@@ -15,14 +18,20 @@ nextButton.addEventListener('click', renderQuestion);
 function runQuiz() {
   startPage.classList.add('hide');
   quizContainer.classList.remove('hide');
-  welcomeHeading.innerHTML = "Choose the correct phrasal verb";
-  document.getElementById('right').textContent = 0;
-  document.getElementById('wrong').textContent = 0;
+  welcomeHeading.innerHTML = "Choose the correct phrasal verb:";
+  /* followed instructions from this video in order to create the render function and display function features
+  https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified*/
+  shuffledQuestions = questions.sort(() => Math.random() - .5);
   currentQuestionIndex = 0;
-  displayNextQuestion();
+  renderQuestion();
 }
 
-function renderQuestion(question) {
+function renderQuestion() {
+  displayQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function displayQuestion(question) {
+  questionElement.innerText = question.question;
 
 }
 
@@ -64,4 +73,4 @@ const questions = [{
     options: ["look up", "fill in", "read up on", "take up"],
     correctAnswer: "fill in"
   },
-]
+];
