042135cd45d08c63d45ece1f71711353da3224bf
HPCarey <hannahpcarey@gmail.com>
2022-07-02 16:54:55 +0000

    feat: rewrite the options as objects and create buttons for each option in display question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@ const startPage = document.getElementById("start-page");
 const quizContainer = document.getElementById("quiz-container");
 const welcomeHeading = document.getElementById("welcome-heading");
 
-let shuffledQuestions
-let currentQuestionIndex
+let shuffledQuestions;
+let currentQuestionIndex;
 
 // event listeners
 startButton.addEventListener('click', runQuiz);
 nextButton.addEventListener('click', renderQuestion);
+
 
 //functions
 function runQuiz() {
@@ -31,8 +32,19 @@ function renderQuestion() {
 }
 
 function displayQuestion(question) {
-  questionElement.innerText = question.question;
-
+  questionElement.innerHTML = question.question;
+  /* creates a button and sets its content as the options in the questions section.
+  source https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified*/
+  question.options.forEach(answer => {
+    const button = document.createElement('button');
+    button.textContent = answer.text;
+    answerButtons.appendChild(button);
+    button.addEventListener('click', checkAnswer);
+    // marks the correct answer as correct in the HTML
+    if (answer.correct) {
+        button.dataset.correct = answer.correct;
+    }
+});
 }
 
 function checkAnswer(event) {
@@ -45,32 +57,56 @@ function incrementScore() {
 // Below are questions, options and answers for quiz
 const questions = [{
     question: "Meaning: Learn a new skill through practice rather than study.",
-    options: ["read up on", "fill in", "pick up", "take up"],
-    correctAnswer: "pick up"
+    options: [
+      { text: 'read up on', correct: false },
+      { text: 'fill in', correct: false },
+      { text: 'pick up', correct: true },
+      { text: 'take up', correct: false }
+    ]
   },
   {
     question: "Meaning: Revise/ study a concept again to clarify the meaning",
-    options: ["look up", "fill in", "read up on", "go over"],
-    correctAnswer: "go over"
+    options: [
+    { text: 'look up', correct: false },
+    { text: 'fill in', correct: false },
+    { text: 'read up on', correct: false },
+    { text: 'go over', correct: true }
+    ]
   },
   {
     question: "Meaning: Use an information database or learning resource to verify something or learn something specific",
-    options: ["read up on", "look up", "pick up", "take up"],
-    correctAnswer: "look up"
+    options: [
+      { text: 'read up on', correct: false },
+      { text: 'look up', correct: true },
+      { text: 'pick up', correct: false },
+      { text: 'take up', correct: false }
+    ]
   },
   {
     question: "Meaning: Start learning a new skill or hobby",
-    options: ["pick up", "read up on", "take up", "go over"],
-    correctAnswer: "take up"
+    options: [
+      { text: 'pick up', correct: false },
+      { text: 'read up on', correct: false },
+      { text: 'take up', correct: true },
+      { text: 'go over', correct: false }
+    ]
   },
   {
     question: "Meaning: Expand your knowledge of a subject through study",
-    options: ["read up on", "look up", "take up", "go over"],
-    correctAnswer: "read up on"
+    options: [
+      { text: 'read up on', correct: true },
+      { text: 'look up', correct: false },
+      { text: 'take up', correct: false },
+      { text: 'go over', correct: false }
+    ]
   },
   {
     question: "Meaning: Complete information that is missing from a form or test",
-    options: ["look up", "fill in", "read up on", "take up"],
-    correctAnswer: "fill in"
-  },
+    options: [
+      { text: 'look up', correct: false },
+      { text: 'fill in', correct: true },
+      { text: 'read up on', correct: false },
+      { text: 'take up', correct: false }
+    ]
+  }
 ];
