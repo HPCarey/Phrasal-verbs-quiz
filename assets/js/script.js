476893a04d88fdeac1164f9b5ee2c63ab09c077c
HPCarey <hannahpcarey@gmail.com>
2022-07-05 15:39:47 +0000

    fix: rename const variable according to mento advice. Put event listeners inside DOMContentLoaded event according to mentor's advice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 // define variables to select elements
-const startButton = document.getElementById("start-quiz-button");
-const nextButton = document.getElementById("next-button");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const startPage = document.getElementById("start-page");
-const quizContainer = document.getElementById("quiz-container");
-const welcomeHeading = document.getElementById("welcome-heading");
-const welcomeText = document.getElementById("welcome-text");
-const restartButton = document.getElementById("restart-quiz-button");
-const endQuizContainer = document.getElementById("end-quiz-container");
 
-let shuffledQuestions;
-let currentQuestionIndex;
+let shuffledQuestions = null;
+let currentQuestionIndex = null;
 
 // event listeners
-startButton.addEventListener('click', runQuiz);
-nextButton.addEventListener('click', renderQuestion);
-restartButton.addEventListener('click', restartQuiz);
+window.addEventListener('DOMContentLoaded', (event) => {
+  let startButton = document.getElementById("start-quiz-button");
+  startButton.addEventListener('click', runQuiz);
+  let nextButton = document.getElementById("next-button");
+  nextButton.addEventListener('click', renderQuestion);
+  let restartButton = document.getElementById("restart-quiz-button");
+  restartButton.addEventListener('click', restartQuiz);
+  console.log('DOM fully loaded and parsed');
+});
+
 
 
 //functions
 function runQuiz() {
+  let startPage = document.getElementById("start-page");
   startPage.classList.add('hide');
+  let quizContainer = document.getElementById("quiz-container");
   quizContainer.classList.remove('hide');
+  let welcomeHeading = document.getElementById("welcome-heading");
   welcomeHeading.innerHTML = "Choose the correct phrasal verb:";
   /* followed instructions from this video in order to create the render function and display function features
   https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified*/
@@ -37,16 +37,17 @@ function renderQuestion() {
   currentQuestionIndex++;
   //nextQuestionIcon.classList.add("greyscale");
   //nextQuestionIcon.setAttribute("disabled", "disabled");
-
 }
 
 function displayQuestion(question) {
+  let questionElement = document.getElementById("question");
   questionElement.innerHTML = question.question;
   /* creates a button and sets its content as the options in the questions section.
   source https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified*/
   question.options.forEach(answer => {
-    const button = document.createElement('button');
+    let button = document.createElement('button');
     button.textContent = answer.text;
+    let answerButtons = document.getElementById("answer-buttons");
     answerButtons.appendChild(button);
     button.addEventListener('click', checkAnswer);
     // marks the correct answer as correct in the HTML
@@ -59,7 +60,9 @@ function displayQuestion(question) {
 function resetState() {
   /* Removes the default answer buttons and replaces with the answers in question array
   source: https://www.youtube.com/watch?v=riDzcEQbX6k&ab_channel=WebDevSimplified*/
+  let nextButton = document.getElementById("next-button");
   nextButton.setAttribute('disabled', '');
+  let answerButtons = document.getElementById("answer-buttons");
   answerButtons.innerHTML = '';
 }
 
@@ -67,23 +70,30 @@ function resetState() {
 function checkAnswer(e) {
   const selectButton = e.target;
   const correct = selectButton.dataset.correct;
+  let answerButtons = document.getElementById("answer-buttons");
   Array.from(answerButtons.children).forEach(button => {
     setStatusClass(button, button.dataset.correct);
   });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    let nextButton = document.getElementById("next-button");
     nextButton.removeAttribute('disabled');
   } else if (shuffledQuestions.length >= questions.length) {
-      nextButton.addEventListener('click', endQuiz());
+    let nextButton = document.getElementById("next-button");
+    nextButton.addEventListener('click', endQuiz());
   }
 }
 
 function endQuiz() {
+  let quizContainer = document.getElementById("quiz-container");
   quizContainer.classList.add('hide');
+  let endQuizContainer = document.getElementById("end-quiz-container");
   endQuizContainer.classList.remove('hide');
 }
 
-function restartQuiz(){
+function restartQuiz() {
+  let startPage = document.getElementById("start-page");
   startPage.classList.remove('hide');
+  let endQuizContainer = document.getElementById("end-quiz-container");
   endQuizContainer.classList.add('hide');
 }
 
