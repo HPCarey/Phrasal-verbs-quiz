25e15cee546982ea87045e3f697f1e4b0a40ba43
HPCarey <hannahpcarey@gmail.com>
2022-07-05 15:45:32 +0000

    fix: create new js file for questions to improve readability of code folloring mentor's advice

---
+++ assets/js/question.js
@@ -0,0 +1,122 @@
+// Below are questions, options and answers for quiz
+const questions = [{
+    question: "Meaning: Learn a new skill through practice rather than study.",
+    options: [{
+        text: 'read up on',
+        correct: false
+      },
+      {
+        text: 'fill in',
+        correct: false
+      },
+      {
+        text: 'pick up',
+        correct: true
+      },
+      {
+        text: 'take up',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: "Meaning: Revise/ study a concept again to clarify the meaning",
+    options: [{
+        text: 'look up',
+        correct: false
+      },
+      {
+        text: 'fill in',
+        correct: false
+      },
+      {
+        text: 'read up on',
+        correct: false
+      },
+      {
+        text: 'go over',
+        correct: true
+      }
+    ]
+  },
+  {
+    question: "Meaning: Use an information database or learning resource to verify something or learn something specific",
+    options: [{
+        text: 'read up on',
+        correct: false
+      },
+      {
+        text: 'look up',
+        correct: true
+      },
+      {
+        text: 'pick up',
+        correct: false
+      },
+      {
+        text: 'take up',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: "Meaning: Start learning a new skill or hobby",
+    options: [{
+        text: 'pick up',
+        correct: false
+      },
+      {
+        text: 'read up on',
+        correct: false
+      },
+      {
+        text: 'take up',
+        correct: true
+      },
+      {
+        text: 'go over',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: "Meaning: Expand your knowledge of a subject through study",
+    options: [{
+        text: 'read up on',
+        correct: true
+      },
+      {
+        text: 'look up',
+        correct: false
+      },
+      {
+        text: 'take up',
+        correct: false
+      },
+      {
+        text: 'go over',
+        correct: false
+      }
+    ]
+  },
+  {
+    question: "Meaning: Complete information that is missing from a form or test",
+    options: [{
+        text: 'look up',
+        correct: false
+      },
+      {
+        text: 'fill in',
+        correct: true
+      },
+      {
+        text: 'read up on',
+        correct: false
+      },
+      {
+        text: 'take up',
+        correct: false
+      }
+    ]
+  }
+];
